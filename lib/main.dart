b5d9cf839866db6e46f73c0cc4095e557aee0859
vngarg <sngargrsd@gmail.com>
2020-04-18 01:10:23 +0530

    Added Home Page Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:corona_check/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,22 +24,112 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+              height: 350,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [Color(0xFF3383CD), Color(0xFF11249F)]),
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/virus.png"))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                      alignment: Alignment.topRight,
+                      child: SvgPicture.asset("assets/icons/menu.svg")),
+                  SizedBox(height: 20.0),
+                  Expanded(
+                    child: Stack(
+                      children: <Widget>[
+                        SvgPicture.asset(
+                          "assets/icons/Drcorona.svg",
+                          width: 230.0,
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.topCenter,
+                        ),
+                        Positioned(
+                          top: 20.0,
+                          left: 150.0,
+                          child: Text("All you need \nis stay at home",
+                              style: kHeadingTextStyle.copyWith(
+                                  color: Colors.white)),
+                        ),
+                        Container(),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
           Container(
-            height: 350,
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            height: 60.0,
             width: double.infinity,
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                  Color(0xFF3383CD),
-                  Color(0xFF11249F)
-                  ]),
-                  image: DecorationImage(image: AssetImage("assets/images/virus.png"))
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25.0),
+                border: Border.all(color: Color(0xFFE5E5E5))),
+            child: Row(
+              children: <Widget>[
+                SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                SizedBox(width: 20.0,),
+                Expanded(
+                  child: DropdownButton(
+                    isExpanded: true,
+                    underline: SizedBox(),
+                    icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                    value: 'Indonesia',
+                    items: ['Indonesia', 'Bangladesh', 'United States', 'Japan']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        child: Text(value),
+                        value: value,
+                      );
+                    }).toList(),
+                    onChanged: (value) {},
                   ),
-          )
+                )
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: <Widget>[
+              Row(children: <Widget>[
+                RichText(text: TextSpan(children: [
+                  TextSpan(text: "Case Update" , style: kTitleTextstyle)
+                ]),)
+              ],)
+            ],
+          ),)
         ],
       ),
     );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
